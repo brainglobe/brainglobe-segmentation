4cf06a72070a323879ff7028c27b7c7926c2a65c
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-08-26 18:08:17 +0100

    test region IO

---
+++ tests/tests/test_unit/test_regions/test_region_IO.py
@@ -0,0 +1,17 @@
+import tifffile
+from filecmp import cmp
+
+from pathlib import Path
+from brainreg_segment.regions import IO as region_IO
+
+
+regions_dir = Path.cwd() / "tests" / "data" / "regions"
+VOXEL_SIZE = 100
+
+
+def test_export_regions_to_file(tmpdir):
+    image = tifffile.imread(regions_dir / "region.tiff")
+    filename = tmpdir / "region.obj"
+    region_IO.export_regions_to_file(image, filename, VOXEL_SIZE)
+
+    cmp(regions_dir / "region.obj", tmpdir / "region.obj")
