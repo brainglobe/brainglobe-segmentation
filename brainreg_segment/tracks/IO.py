f0c0e33bb369be6e10650049c1b2d15b3e5c74b5
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-08-26 15:04:08 +0100

    test track IO

--- brainreg_segment/tracks/IO.py
+++ brainreg_segment/tracks/IO.py
@@ -8,18 +8,19 @@ def save_track_layers(
     tracks_directory.mkdir(parents=True, exist_ok=True)
 
     for points_layer in points_layers:
-        save_single_track_layer(
-            points_layer,
+        save_single_track(
+            points_layer.data,
+            points_layers.name,
             tracks_directory,
             track_file_extension=track_file_extension,
         )
 
 
-def save_single_track_layer(
-    layer, output_directory, track_file_extension=".points",
+def save_single_track(
+    points, name, output_directory, track_file_extension=".points",
 ):
-    output_filename = output_directory / (layer.name + track_file_extension)
-    points = pd.DataFrame(layer.data)
+    output_filename = output_directory / (name + track_file_extension)
+    points = pd.DataFrame(points)
     points.to_hdf(output_filename, key="df", mode="w")
 
 
