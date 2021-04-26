a1abc964a31d0f5b5770c896cb2c0b3164838adb
Adam Tyson <code@adamltyson.com>
2021-04-26 14:16:26 +0100

    Bump version: 0.2.7-rc0 → 0.2.7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name="brainreg-segment",
-    version="0.2.7-rc0",
+    version="0.2.7",
     description="Manual segmentation of 3D brain structures in a common anatomical space",
     long_description=long_description,
     long_description_content_type="text/markdown",
