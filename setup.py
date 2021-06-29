3d22f74313944209df0790a7abc877bfc4ea9c65
Adam Tyson <code@adamltyson.com>
2021-06-29 12:03:19 +0100

    Bump version: 0.2.9-rc0 → 0.2.9

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name="brainreg-segment",
-    version="0.2.9-rc0",
+    version="0.2.9",
     author="Adam Tyson, Horst Obenhaus",
     author_email="code@adamltyson.com",
     license="BSD-3-Clause",
