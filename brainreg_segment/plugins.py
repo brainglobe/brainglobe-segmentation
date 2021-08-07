fac3bfc2f21559136334edf115d64b077d13a749
juanma9613 <jmunozb@eafit.edu.co>
2021-08-07 10:24:36 -0500

    solved bug of layers name

--- brainreg_segment/plugins.py
+++ brainreg_segment/plugins.py
@@ -1,20 +1,23 @@
 from napari_plugin_engine import napari_hook_implementation
 from brainreg_segment.segment import SegmentationWidget
 from brainreg_segment.measurement import measurement
-from brainreg_segment.measurement.layers import (generate_random_points_2d_layer,
-                                                 generate_random_points_3d_layer)
+from brainreg_segment.measurement.random_layers import (
+    generate_random_points_2d_layer, generate_random_points_3d_layer)
+
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return SegmentationWidget, {"name": "Manual segmentation"}
+  return SegmentationWidget, {"name": "Manual segmentation"}
+
 
 @napari_hook_implementation
 def napari_experimental_provide_function():
-    return measurement.analyze_points_layer
+  return measurement.analyze_points_layer
+
 
 @napari_hook_implementation
 def napari_provide_sample_data():
-    return {
-        'random 3d points': generate_random_points_3d_layer,
-        'random 2d points': generate_random_points_2d_layer
-    }
+  return {
+      'random 3d points': generate_random_points_3d_layer,
+      'random 2d points': generate_random_points_2d_layer
+  }
