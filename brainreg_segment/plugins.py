0150ee4319b0e7a64ffd37bc63e5821c24ec44a0
Adam Tyson <code@adamltyson.com>
2023-10-27 10:06:38 +0100

    Remove measurement code (#135)
    * Remove measurement code
    
    * Renames widget

--- brainreg_segment/plugins.py
+++ brainreg_segment/plugins.py
@@ -1,24 +1,10 @@
 from napari_plugin_engine import napari_hook_implementation
 
-from brainreg_segment.measurement import measurement
-from brainreg_segment.measurement.random_layers import (
-    generate_random_points_2d_layer,
-    generate_random_points_3d_layer,
-)
 from brainreg_segment.segment import SegmentationWidget
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     return [
-        (SegmentationWidget, {"name": "Manual segmentation"}),
-        (measurement.analyze_points_layer, {"name": "Analyze points layer"}),
+        (SegmentationWidget, {"name": "Region/track segmentation"}),
     ]
-
-
-@napari_hook_implementation
-def napari_provide_sample_data():
-    return {
-        "random 3d points": generate_random_points_3d_layer,
-        "random 2d points": generate_random_points_2d_layer,
-    }
