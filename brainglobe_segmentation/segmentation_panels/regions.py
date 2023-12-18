4fb50109b979d06ab2a975c9a5e2202969c52640
Adam Tyson <code@adamltyson.com>
2023-12-18 13:23:55 +0000

    remove unecessary comments

--- brainglobe_segmentation/segmentation_panels/regions.py
+++ brainglobe_segmentation/segmentation_panels/regions.py
@@ -121,9 +121,6 @@ class RegionSeg(QGroupBox):
         self.region_panel.setVisible(False)
 
     def toggle_region_panel(self):
-        # TODO: Change color scheme directly when theme is switched
-        # TODO: "text-align" property should follow constant
-        # SEGM_METHODS_PANEL_ALIGN
         if self.region_panel.isVisible():
             self.region_panel.setVisible(False)
             if self.parent.viewer.theme == "dark":
